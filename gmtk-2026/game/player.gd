2393771bd408502093f8e588a046ea6cc1f88de4
tyler_hansen <tjhans128@gmail.com>
2026-07-23 12:20:48 -0700

    Free look and slightly better torch physics

--- gmtk-2026/game/player.gd
+++ gmtk-2026/game/player.gd
@@ -4,7 +4,6 @@ const horizontal_sway_speed = 0.125
 const vertical_sway_speed = 0.025
 const max_horizontal_distance = 0.5
 const max_vertical_distance = 0.15
-const added_vertical_distance = 0.6
 
 var motion_time = 0
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -13,11 +12,12 @@ var jump_speed = 5
 var mouse_sensitivity = 0.002
 var heldTorch : Torch
 
+const torchScene := preload("res://game/Torch.tscn")
+
 @export var player_pcam: PhantomCamera3D = null
 
 func _ready():
-	#TODO - load the first torch dynamically and create a reloader
-	heldTorch = $TorchOffset/Torch
+	spawn_torch()
 
 func _physics_process(delta):
 	velocity.y += -gravity * delta
@@ -31,20 +31,23 @@ func _physics_process(delta):
 		velocity.y = jump_speed
 
 	# update camera position
-	if is_instance_valid(player_pcam):
-		player_pcam.set_third_person_rotation(rotation)
+	#if is_instance_valid(player_pcam):
+		#player_pcam.set_third_person_rotation(rotation)
 
 	# torch sway
 func _process(delta: float) -> void:
 	motion_time += delta
-	var offset_x = sin(motion_time*horizontal_sway_speed*TAU)*max_horizontal_distance
-	$TorchOffset.position.x = offset_x
-	var offset_y = sin(motion_time*vertical_sway_speed*TAU)*max_vertical_distance
-	$TorchOffset.position.y = offset_y + added_vertical_distance
+	if is_instance_valid(heldTorch):
+		var offset_x = sin(motion_time*horizontal_sway_speed*TAU)*max_horizontal_distance
+		heldTorch.position.x = offset_x
+		var offset_y = sin(motion_time*vertical_sway_speed*TAU)*max_vertical_distance
+		heldTorch.position.y = offset_y
 	
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Head.rotation.x = clamp($Head.rotation.x + (-event.relative.y * mouse_sensitivity), -PI/2, PI/2)
+		print($Head.rotation.x)
 	elif event is InputEventMouseButton:
 		if event.pressed:
 			throw_torch()
@@ -55,5 +58,20 @@ func throw_torch():
 		return
 	heldTorch.top_level = true
 	heldTorch.freeze = false
-	heldTorch.linear_velocity = transform.basis.z * -10
+	heldTorch.linear_velocity = $Head.global_basis.z * -10
+	heldTorch.angular_velocity = $Head.global_basis.x * -10
 	heldTorch = null
+	$TorchReloadTimer.start()
+
+
+func _on_torch_reload_timer_timeout() -> void:
+	spawn_torch()
+
+func spawn_torch():
+	if heldTorch != null:
+		push_warning("Attempted to load torch when one was already held")
+		return
+	heldTorch = torchScene.instantiate()
+	%TorchOffset.add_child(heldTorch)
+	
+	
--- gmtk-2026/game/torch.gd
+++ gmtk-2026/game/torch.gd
@@ -1,2 +1,2 @@
-extends Node3D
+extends RigidBody3D
 class_name Torch
