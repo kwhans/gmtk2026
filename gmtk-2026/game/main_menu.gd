f018ea72c2af00157a876cfebaa68213ef190ebc
Kyle Hansen <kwhans@gmail.com>
2026-07-25 21:15:49 -0600

    added a new level

--- gmtk-2026/game/main_menu.gd
+++ gmtk-2026/game/main_menu.gd
@@ -4,6 +4,6 @@ func _ready() -> void:
 	SignalBus.start_main_game.connect(on_start_main_game)
 	
 func on_start_main_game() -> void:
-	GlobalGameState.starting_level = 1
+	#GlobalGameState.starting_level = 1
 	get_tree().change_scene_to_file("res://game/LoadingScreen.tscn")
 	
